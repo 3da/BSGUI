70681d291b31e0a311c9f2d919c691ac3d678ff7
Alexey <3da@rambler.ru>
2011-02-07 20:42:32 +0600

    Demo project improved

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -20,49 +20,46 @@ Label	*fps;
 
 BMFont *font;
 
-Theme theme;
+Theme themeDefault;
+Theme themeRed;
 
-void stepsModifiedAction(Control *sender)
+void SetThemeRed(Control *sender)
+{
+	sender->parent->SetTheme(themeRed);
+}
+
+void SetThemeDefault(Control *sender)
+{
+	sender->parent->SetTheme(themeDefault);
+}
+
+void StepsModifiedAction(Control *sender)
 {
 	ProgressBar	*pbar2 = (ProgressBar*)Screen::screen->FindChild("pbar2");
 	steps = (int)stepCount->value + 3;
 	pbar2->value = steps - 3;
 }
 
-void terminateAppAction(Control *sender)
+void TerminateAppAction(Control *sender)
 {
 	running = false;
 }
 
-void closeWindowAction(Control *sender)
-{
-	delete sender->parent;
-}
 
-void newWindowAction(Control *sender)
-{
-	Window	*w = new Window(Screen::screen, theme, "A simple window");
-	w->Place(10, 10, 300, 100);
-	w->Move(rand()%600+30, rand()%500+30);
-
-	Button	*button = new Button(w, theme, 10, 20, 280, 55, "Close me");
-	button->clicked = new CallbackAction(closeWindowAction);
-}
-
-void addWideItemAction(Control *sender)
+void AddWideItemAction(Control *sender)
 {
 	Listbox *lbox = (Listbox*)Screen::screen->FindChild("listbox");
 	lbox->AddItem("A very wide item to test horizontal scrolling");
 	//lbox->Clear();
 }
 
-void removeItemAction(Control *sender)
+void RemoveItemAction(Control *sender)
 {
 	Listbox *lbox = (Listbox*)Screen::screen->FindChild("listbox");
 	lbox->RemoveItem(lbox->active);
 }
 
-void addTheAboveTextAction(Control *sender)
+void AddTheAboveTextAction(Control *sender)
 {
 	Listbox *lbox = (Listbox*)Screen::screen->FindChild("listbox");
 	lbox->AddItem(((Passwordbox*)Screen::screen->FindChild("inputbox"))->text.c_str());
@@ -70,17 +67,17 @@ void addTheAboveTextAction(Control *sender)
 	Screen::screen->FindChild("inputbox")->Focus();
 }
 
-void activateScreen1(Control *sender)
+void ActivateScreen1(Control *sender)
 {
 	screen1->Activate();
 }
 
-void activateScreen2(Control *sender)
+void ActivateScreen2(Control *sender)
 {
 	screen2->Activate();
 }
 
-void renderObject()
+void RenderObject()
 {
 	glDisable(GL_CULL_FACE);
 	glBegin(GL_TRIANGLES);
@@ -107,58 +104,56 @@ void renderObject()
 	glEnable(GL_CULL_FACE);
 }
 
-void renderObjectAction(Control *sender)
+void RenderObjectAction(Control *sender)
 {
 	glTranslatef(0.0f, 0.0f, -5.0f);
 	glRotatef(rotation, 0.0f, 1.0f, 0.0f);
-	renderObject();
+	RenderObject();
 }
 
-void fileSelectedAction(Control *sender)
+void FileSelectedAction(Control *sender)
 {
 	const char *fname = ((FileBrowser*)sender)->GetFilename();
 	printf("File selected: %s\n", fname);
 }
 
-void selectFileAction(Control *sender)
+void SelectFileAction(Control *sender)
 {
-	FileBrowser	*browser = new FileBrowser(theme, Screen::screen, "Select File");
-	browser->selected = new CallbackAction(fileSelectedAction);
+	FileBrowser	*browser = new FileBrowser(themeDefault, Screen::screen, "Select File");
+	browser->selected = FileSelectedAction;
 }
 
-void aboutBoxAction(Control *sender)
+void AboutBoxAction(Control *sender)
 {
-	MessageBox(theme, "BSGUI (Bad Sector's OpenGL GUI) version 0.2 Demo", "About");
+	MessageBox(sender->theme, "BSGUI demo", "About");
 }
 
-void imageWinResizedAction(Control *sender)
+void ImageWinResizedAction(Control *sender)
 {
-	//Image	*img = (Image*)*sender->childs.begin();
-	//int	w, h;
-	//sender->GetClientSize(w, h);
-	//img->minWidth = 10;
-	//img->minHeight = 10;
-	//img->Place(img->x1, img->y1, w-img->x1 - 1, h-img->y1 - 1);
+	Picture *img = (Picture*)sender->FindChild("bob");
+	int	w, h;
+	sender->GetClientSize(w, h);
+	img->Place(10, 10, w - 10, h - 30);
 }
 
-void enableFiltering(Control *sender)
+void EnableFiltering(Control *sender)
 {
 	Image	*img = (Image*)Screen::screen->FindChild("cenda");
 	//img->bitmap->SetFiltering(true);
 }
 
-void disableFiltering(Control *sender)
+void DisableFiltering(Control *sender)
 {
 	Image	*img = (Image*)Screen::screen->FindChild("cenda");
 	//img->bitmap->SetFiltering(false);
 }
 
-void cendaClicked(Control *sender)
+void CendaClicked(Control *sender)
 {
-	selectFileAction(sender);
+	SelectFileAction(sender);
 }
 
-void render()
+void Render()
 {
 	//RenderOffscreenBSGUIControls();
 
@@ -171,16 +166,16 @@ void render()
 	glRotatef(rotation, 1.0f, 0.0f, 0.0f);
 	glRotatef(rotation/0.6f, 0.0f, 1.0f, 0.0f);
 
-	renderObject();
+	RenderObject();
 
- 	Render();
+ 	BSGUI::Render();
 
   	SDL_GL_SwapBuffers();
 
 	progress += 0.008;
 }
 
-void initOpenGL()
+void InitOpenGL()
 {
 	SDL_Init(SDL_INIT_VIDEO);
 	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
@@ -206,43 +201,51 @@ void initOpenGL()
 	glMatrixMode(GL_MODELVIEW);
 }
 
-void createUI()
+void CreateUI()
 {
-	IMG_Init(IMG_INIT_PNG);
+	IMG_Init(IMG_INIT_PNG|IMG_INIT_JPG);
 	font = new BMFont("droid30.fnt");
 	font->Init();
-	theme.font = font;
-	theme.fontSize = 0.5;
-	theme.height = 20;
-	theme.scrollSize = 10;
-	//Draw::LoadFontData("fontdata.bmp", "fontsize.dat");
-	//Draw::LoadCursorImage("cursor.bmp", "cursorAlpha.bmp");
-	Draw::SetCursorImage(ImageOpenGL::LoadImage("SoS2.png"));
+	themeDefault.font = font;
+	themeDefault.fontSize = 0.5;
+	themeDefault.height = 20;
+	themeDefault.scrollSize = 10;
+	themeRed.font = font;
+	themeRed.fontSize = 0.7;
+	themeRed.height = 35;
+	themeRed.scrollSize = 15;
+	themeRed.colorFrameFocused.Set(0.7, 0.3, 0.2, 0.5);
+	themeRed.colorFrameHovered.Set(0.8, 0.2, 0.3, 0.5);
+	themeRed.colorFrameNormal.Set(0.65, 0.3, 0.3, 0.5);
+	Draw::SetCursorImage(ImageOpenGL::LoadImage("cursor.png"));
+	Draw::SetCursorAlign(IF_ALIGN_CENTER);
 
-	win = new Window(Screen::screen, theme, "A fluffy window");
+	screen1 = new Screen(themeDefault);
+
+
+	win = new Window(screen1, themeDefault, "Unmovable window");
 	win->Center();
+	win->movable = false;
 
-	screen1 = new Screen(theme);
-	screen1->Activate();
 
- 	new Label(win, theme, 5, 0, "A Label");
 
- 	(new Button(win, theme, 10, 25, 180, 50, "Create window"))->clicked =
-		new CallbackAction(newWindowAction);
- 	new Button(win, theme, 10, 55, 180, 80, "Another button");
- 	new Button(win, theme, 10, 85, 180, 110, "Yet another button");
+ 	new Label(win, themeDefault, 5, 0, "A Label");
 
-	Window	*win2 = new Window(Screen::screen, theme, "Another fluffy window");
+ 	(new Button(win, themeDefault, 10, 25, 180, 50, "About"))->clicked = AboutBoxAction;
+ 	(new Button(win, themeDefault, 10, 55, 180, 80, "Use red theme"))->clicked = SetThemeRed;
+ 	(new Button(win, themeDefault, 10, 85, 180, 110, "Use default theme"))->clicked = SetThemeDefault;
+
+	Window	*win2 = new Window(screen1, themeDefault, "Fluffy window");
 	win2->Center();
 	win2->Move(win2->x1 + 200, win2->y1 - 70);
 	win2->Resize(win2->x2-win2->x1+1, win2->y2-win2->y1+270);
-	win2->movable = false;
+	win2->movable = true;
 
-	(new Checkbox(win2, theme, 10, 0, 180, 20, "Checker"))->checked = true;
-	new Checkbox(win2, theme, 10, 25, 180, 45, "Checker 2");
-	new Checkbox(win2, theme, 10, 50, 180, 70, "Checker 3");
+	(new Checkbox(win2, themeDefault, 10, 0, 180, 20, "Checker"))->checked = true;
+	new Checkbox(win2, themeDefault, 10, 25, 180, 45, "Checker 2");
+	new Checkbox(win2, themeDefault, 10, 50, 180, 70, "Checker 3");
 
-	Listbox *lbox = new Listbox(win2, theme, 10, 80, 175, 200);
+	Listbox *lbox = new Listbox(win2, themeDefault, 10, 80, 175, 200);
 	lbox->AddItem("A string item");
 	for (int i=0;i<20;i++)
 	{
@@ -251,47 +254,45 @@ void createUI()
 		lbox->AddItem(buff);
 	}
 	lbox->name = "listbox";
-	Button *b1 = (new Button(win2, theme, 10, 210, 175, 235, "Add wide item"));//
-	b1->clicked = new CallbackAction(addWideItemAction);
+	Button *b1 = (new Button(win2, themeDefault, 10, 210, 175, 235, "Add wide item"));//
+	b1->clicked = AddWideItemAction;
 	b1->visible = true;
-	(new Button(win2, theme, 10, 240, 175, 265, "Remove active item"))->clicked =
-	        new CallbackAction(removeItemAction);
-	(new Passwordbox(win2, theme, 10, 270, 175, 295))->name = "inputbox";
-	(new Button(win2, theme, 10, 300, 175, 325, "Add the above text"))->clicked =
-	        new CallbackAction(addTheAboveTextAction);
+	(new Button(win2, themeDefault, 10, 240, 175, 265, "Remove active item"))->clicked = RemoveItemAction;
+	(new Passwordbox(win2, themeDefault, 10, 270, 175, 295))->name = "inputbox";
+	(new Button(win2, themeDefault, 10, 300, 175, 325, "Add the above text"))->clicked = AddTheAboveTextAction;
 
-	ProgressBar	*pbar = new ProgressBar(win2, theme, 10, 330, 175, 355);
+	ProgressBar	*pbar = new ProgressBar(win2, themeDefault, 10, 330, 175, 355);
 	pbar->value = 10;
-	ProgressBar	*pbar2 = new ProgressBar(win2, theme, 10, 360, 175, 385);
+	ProgressBar	*pbar2 = new ProgressBar(win2, themeDefault, 10, 360, 175, 385);
 	pbar2->name = "pbar2";
 	pbar2->max = 62;
 
-	Window	*win3 = new Window(Screen::screen, theme, "A window");
+	Window	*win3 = new Window(screen1, themeDefault, "A window");
 	win3->Center();
 	win3->Move(win3->x1 - 200, win3->y1 - 70);
 	win3->Resize(190, 400);
 
- 	new Label(win3, theme, 5, 25, "Object resolution:");
-	stepCount = new Slider(win3, theme, 10, 50, 180, 65);
+ 	new Label(win3, themeDefault, 5, 25, "Object resolution:");
+	stepCount = new Slider(win3, themeDefault, 10, 50, 180, 65);
 	stepCount->max = 61;
 	stepCount->value = 3;
-	stepCount->modified = new CallbackAction(stepsModifiedAction);
+	stepCount->modified = StepsModifiedAction;
 
- 	new Label(win3, theme, 5, 80, "Object size:");
-	sizeSlider = new Slider(win3, theme, 10, 105, 180, 120);
+ 	new Label(win3, themeDefault, 5, 80, "Object size:");
+	sizeSlider = new Slider(win3, themeDefault, 10, 105, 180, 120);
 	sizeSlider->max = 5;
 	sizeSlider->value = 30;
 
- 	new Label(win3, theme, 5, 175, "Background intensity:");
-	backSlider = new Slider(win3, theme, 10, 200, 180, 215);
+ 	new Label(win3, themeDefault, 5, 175, "Background intensity:");
+	backSlider = new Slider(win3, themeDefault, 10, 200, 180, 215);
 	backSlider->max = 1;
 	backSlider->value = 45;
 
- 	Scrollbox	*sbox = new Scrollbox(win3, theme, 10, 240, 175, 350);
- 	new Button(sbox, theme, 10, 10, 200, 35, "Clipped button");
- 	new Button(sbox, theme, 10, 40, 200, 65, "Another clipped button");
- 	new Checkbox(sbox, theme, 10, 80, 200, 100, "Clipped checkbox");
- 	new Label(sbox, theme, 10, 120, "Clipped label");
+ 	Scrollbox	*sbox = new Scrollbox(win3, themeDefault, 10, 240, 175, 350);
+ 	new Button(sbox, themeDefault, 10, 10, 200, 35, "Clipped button");
+ 	new Button(sbox, themeDefault, 10, 40, 200, 65, "Another clipped button");
+ 	new Checkbox(sbox, themeDefault, 10, 80, 200, 100, "Clipped checkbox");
+ 	new Label(sbox, themeDefault, 10, 120, "Clipped label");
 
 
 	//Window	*win4 = new Window(Screen::screen, "ModelView control");
@@ -301,10 +302,18 @@ void createUI()
 	//ModelView *mview = new ModelView(win4, 10, 10, 175, 110);
 	//mview->renderView = new CallbackAction(renderObjectAction);
 
-	Window *win5 = new Window(Screen::screen, theme, "Image control");
+	Window *win5 = new Window(screen1, themeDefault, "Image control");
 	win5->Center();
 	win5->Move(win5->x1, win5->y1 - 160);
-	//Image *cenda = ImageOpenGL::LoadImage("cenda.bmp");
+	Image *img = ImageOpenGL::LoadImage("spongebob.jpg");
+	img->GetHeight();
+	int width, height;
+	win5->GetClientSize(width, height);
+	win5->resizeable = true;
+	win5->resized = ImageWinResizedAction;
+	Picture *bob = new Picture(win5, themeDefault, 10,10, width-10, height-30, img);
+	bob->name = "bob";
+
 	//cenda->Center();
 	//cenda->name = "cenda";
 	//cenda->clicked = new CallbackAction(cendaClicked);
@@ -313,39 +322,37 @@ void createUI()
 	//win5->icon = new Bitmap(INTERNALBMP_WINICON);
 	win5->movable = false;
 
-	(new Button(Screen::screen, theme, 750, 5, 795, 30, "Quit"))->clicked = new CallbackAction(terminateAppAction);
+	(new Button(Screen::screen, themeDefault, 750, 5, 795, 30, "Quit"))->clicked = TerminateAppAction;
 
 
 
 
-	screen2 = new Screen(theme);
+	screen2 = new Screen(themeDefault);
 	//Window	*nwin = new Window(screen2, "A window in screen 2");
 	//nwin->Center();
 
-	BSGUI::Window *winConnecting = new BSGUI::Window(screen2, theme, "Connecting");
+	BSGUI::Window *winConnecting = new BSGUI::Window(screen2, themeDefault, "Connecting");
 	winConnecting->Resize(200,150);
 	winConnecting->Center();
 	winConnecting->movable = false;
 
-	new BSGUI::Label(winConnecting, theme, 10, 10, "Connecting");
+	new BSGUI::Label(winConnecting, themeDefault, 10, 10, "Connecting");
 
 
 
-	(new BSGUI::Button(winConnecting, theme, 30, 80, 170, 110, "Quit"));
+	(new BSGUI::Button(winConnecting, themeDefault, 30, 80, 170, 110, "Quit"));
 
- 	(new Button(Screen::screen, theme, 5, 5, 105, 30, "Screen 1"))->clicked =
- 		new CallbackAction(activateScreen1);
- 	(new Button(Screen::screen, theme, 110, 5, 210, 30, "Screen 2"))->clicked =
- 		new CallbackAction(activateScreen2);
- 	(new Button(screen2, theme, 5, 5, 105, 30, "Screen 1"))->clicked =
- 		new CallbackAction(activateScreen1);
- 	(new Button(screen2, theme, 110, 5, 210, 30, "Screen 2"))->clicked =
- 		new CallbackAction(activateScreen2);
+ 	(new Button(Screen::screen, themeDefault, 5, 5, 105, 30, "Screen 1"))->clicked = ActivateScreen1;
+ 	(new Button(Screen::screen, themeDefault, 110, 5, 210, 30, "Screen 2"))->clicked = ActivateScreen2;
+ 	(new Button(screen2, themeDefault, 5, 5, 105, 30, "Screen 1"))->clicked = ActivateScreen1;
+ 	(new Button(screen2, themeDefault, 110, 5, 210, 30, "Screen 2"))->clicked = ActivateScreen2;
 
- 	fps = new Label(screen1, theme, 5, 40, "?? fps ");
+ 	fps = new Label(screen1, themeDefault, 5, 40, "?? fps ");
+
+ 	screen1->Activate();
 }
 
-void runMe()
+void RunMe()
 {
 	SDL_Event	event;
 	uint		last = SDL_GetTicks();
@@ -377,17 +384,17 @@ void runMe()
 					break;
 				}
 		}
-		render();
+		::Render();
 	}
 }
 
 int main(int argc, char **argv)
 {
-	initOpenGL();
+	InitOpenGL();
 
-	createUI();
+	CreateUI();
 
-	runMe();
+	RunMe();
 
 	SDL_Quit();
 
